7890120624729d79b774b3337eb18e379f53a30c
gilr00y <jas.gilbertson@gmail.com>
2026-01-14 16:25:09 -0600

    Initial Commit

---
+++ grassy/grassyApp.swift
@@ -0,0 +1,17 @@
+//
+//  grassyApp.swift
+//  grassy
+//
+//  Created by jason on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct grassyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
